e5a052a3f3319e2416df54fa4d96271dda83f30c
Okuu <johnnyasdf@hotmail.com>
2026-07-12 18:17:36 +0200

    OmegaPatchy mod: gives Patchy all of her spells

---
+++ OmegaPatchy/patch.js
@@ -0,0 +1,6 @@
+{
+    "title": "OmegaPatchy",
+    "servers": [
+        "https://raw.githubusercontent.com/RealOkuu/RealOkuu_2hu_mods/master/OmegaPatchy/"
+    ]
+}
---
+++ repo.js
@@ -0,0 +1,17 @@
+{
+    "id": "RealOkuu",
+    "title": "RealOkuu 2hu mods",
+    "contact": "",
+    "patches": {
+        "OmegaPatchy": "Patchouli always plays every spellcard"
+    },
+    "patchdata": {
+        "OmegaPatchy": {
+            "flags": "gameplay",
+            "games": ["th06"]
+        }
+    },
+    "servers": [
+        "https://raw.githubusercontent.com/RealOkuu/RealOkuu_2hu_mods/master/"
+    ]
+}
