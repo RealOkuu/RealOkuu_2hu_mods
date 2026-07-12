8f0f7cb6f258d0afe9a918706b3b998bd4ce920f
Okuu <johnnyasdf@hotmail.com>
2026-07-12 20:26:48 +0200

    MagicalAstronomyNecrofantasia mod: replaces Necrofantasia in TH07 with the version from the Magical Astronomy album

---
+++ MagicalAstronomyNecrofantasia/patch.js
@@ -0,0 +1,8 @@
+{
+  "id": "MagicalAstronomyNecrofantasia",
+  "servers": [
+    "https://raw.githubusercontent.com/RealOkuu/RealOkuu_2hu_mods/master/MagicalAstronomyNecrofantasia/"
+  ],
+  "title": "Magical Astronomy Necrofantasia",
+  "dependencies":[]
+}
--- repo.js
+++ repo.js
@@ -6,6 +6,7 @@
         "https://raw.githubusercontent.com/RealOkuu/RealOkuu_2hu_mods/master/"
     ],
     "patches": {
-        "OmegaPatchy": "Patchouli always uses all her existing spellcards"
+        "OmegaPatchy": "Patchouli always uses all her existing spellcards",
+        "MagicalAstronomyNecrofantasia": "Replaces Necrofantasia with a looped version of the Magical Astronomy album version"
     }
 }
