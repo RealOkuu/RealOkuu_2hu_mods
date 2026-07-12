d685378a764b5840e0e631a4f77b2f1471b808bc
Okuu <johnnyasdf@hotmail.com>
2026-07-12 18:42:09 +0200

    minor changes

--- OmegaPatchy/patch.js
+++ OmegaPatchy/patch.js
@@ -1,6 +1,6 @@
 {
-    "title": "TH06 OmegaPatchy",
-    "servers": [
-        "https://raw.githubusercontent.com/RealOkuu/RealOkuu_2hu_mods/master/OmegaPatchy/"
-    ]
+  "servers": [
+    "https://raw.githubusercontent.com/RealOkuu/RealOkuu_2hu_mods/master/OmegaPatchy/"
+  ],
+  "title": "TH06 OmegaPatchy"
 }
--- repo.js
+++ repo.js
@@ -2,16 +2,10 @@
     "id": "RealOkuu",
     "title": "RealOkuu 2hu mods",
     "contact": "discord@nucelarraven",
-    "patches": {
-        "OmegaPatchy": "Patchouli always uses all her existing spellcards"
-    },
-    "patchdata": {
-        "OmegaPatchy": {
-            "flags": "gameplay",
-            "games": ["th06"]
-        }
-    },
     "servers": [
         "https://raw.githubusercontent.com/RealOkuu/RealOkuu_2hu_mods/master/"
-    ]
+    ],
+    "patches": {
+        "OmegaPatchy": "Patchouli always uses all her existing spellcards"
+    }
 }
