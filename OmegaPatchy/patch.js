6e6685768fc06b99c7c807d84098a5c7f0b2be54
Okuu <johnnyasdf@hotmail.com>
2026-07-12 19:59:16 +0200

    minor fixes

--- OmegaPatchy/patch.js
+++ OmegaPatchy/patch.js
@@ -1,6 +1,8 @@
 {
+  "id": "OmegaPatchy",
   "servers": [
     "https://raw.githubusercontent.com/RealOkuu/RealOkuu_2hu_mods/master/OmegaPatchy/"
   ],
-  "title": "TH06 OmegaPatchy"
+  "title": "OmegaPatchy",
+  "dependencies":[]
 }
